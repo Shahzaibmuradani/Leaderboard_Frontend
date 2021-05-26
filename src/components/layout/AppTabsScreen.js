8b47e72e567e635ac667fe0ae9ce742c65d9b687
Shahzaib Muradani <shahzaib_muradani@hotmail.com>
2021-05-27 03:46:51 +0500

    initial

--- src/components/layout/AppTabsScreen.js
+++ src/components/layout/AppTabsScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // import {connect} from 'react-redux';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import {SafeAreaView} from 'react-native';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
@@ -15,88 +15,99 @@ import Relevant from '../Tabs/Relevant';
 import {connect} from 'react-redux';
 import MyPosts from '../Tabs/MyPosts';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
+import Spinner from '../layout/Spinner';
 // import {loadUser} from '../../actions/auth';
 
 //const AppTabs = createBottomTabNavigator();
 const AppTabs = createMaterialBottomTabNavigator();
 
-const AppTabsScreen = ({auth: {user}, navigation}) => {
+const AppTabsScreen = ({auth: {loading, user}, navigation}) => {
   return (
     <>
-      <SafeAreaView style={{flex: 1}}>
-        <DrawerIcon navigation={navigation} />
-        <AppTabs.Navigator barStyle={{backgroundColor: '#0C6CD5'}}>
-          <AppTabs.Screen
-            name="allPosts"
-            component={allPosts}
-            options={{
-              tabBarLabel: 'Home',
-              tabBarIcon: (props) => (
-                <MaterialCommunityIcons
-                  name="home-outline"
-                  color={props.color}
-                  size={20}
-                />
-              ),
-            }}
-          />
-          <AppTabs.Screen
-            name="Home"
-            component={Home}
-            options={{
-              tabBarLabel: 'Profile',
-              tabBarIcon: (props) => (
-                <MaterialCommunityIcons
-                  name="account-outline"
-                  color={props.color}
-                  size={20}
-                />
-              ),
-            }}
-          />
-          {user.status === 'Admin' ? (
+      {!loading && user === null ? (
+        <Spinner />
+      ) : (
+        <SafeAreaView style={{flex: 1}}>
+          <DrawerIcon user={user} navigation={navigation} />
+          <AppTabs.Navigator barStyle={{backgroundColor: '#0C6CD5'}}>
             <AppTabs.Screen
-              name="Irrelevant"
-              component={Relevant}
+              name="allPosts"
+              component={allPosts}
               options={{
-                tabBarLabel: 'Irrelevant',
-                tabBarIcon: (props) => (
-                  <FontAwesome name="tasks" color={props.color} size={20} />
-                ),
-              }}
-            />
-          ) : user.status === 'Student' ? (
-            <AppTabs.Screen
-              name="Help"
-              component={Help}
-              options={{
-                tabBarLabel: 'Help',
+                tabBarLabel: 'Home',
                 tabBarIcon: (props) => (
                   <MaterialCommunityIcons
-                    name="help-circle-outline"
+                    name="home-outline"
                     color={props.color}
                     size={20}
                   />
                 ),
               }}
             />
-          ) : (
             <AppTabs.Screen
-              name="MyPosts"
-              component={MyPosts}
+              name="Home"
+              component={Home}
               options={{
-                tabBarLabel: 'My Posts',
+                tabBarLabel: 'Profile',
                 tabBarIcon: (props) => (
-                  <FontAwesome name="tasks" color={props.color} size={20} />
+                  <MaterialCommunityIcons
+                    name="account-outline"
+                    color={props.color}
+                    size={20}
+                  />
                 ),
               }}
             />
-          )}
-        </AppTabs.Navigator>
-      </SafeAreaView>
+            {user && user.status === 'Admin' && (
+              <AppTabs.Screen
+                name="Irrelevant"
+                component={Relevant}
+                options={{
+                  tabBarLabel: 'Irrelevant',
+                  tabBarIcon: (props) => (
+                    <FontAwesome name="tasks" color={props.color} size={20} />
+                  ),
+                }}
+              />
+            )}
+            {user && user.status === 'Student' && (
+              <AppTabs.Screen
+                name="Help"
+                component={Help}
+                options={{
+                  tabBarLabel: 'Help',
+                  tabBarIcon: (props) => (
+                    <MaterialCommunityIcons
+                      name="help-circle-outline"
+                      color={props.color}
+                      size={20}
+                    />
+                  ),
+                }}
+              />
+            )}
+            {user &&
+              (user.status === 'Recruiter' || user.status === 'Organizer') && (
+                <AppTabs.Screen
+                  name="MyPosts"
+                  component={MyPosts}
+                  options={{
+                    tabBarLabel: 'My Posts',
+                    tabBarIcon: (props) => (
+                      <FontAwesome name="tasks" color={props.color} size={20} />
+                    ),
+                  }}
+                />
+              )}
+          </AppTabs.Navigator>
+        </SafeAreaView>
+      )}
     </>
   );
+};
+
+AppTabsScreen.propTypes = {
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
--- src/components/layout/DrawerIcon.js
+++ src/components/layout/DrawerIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
-import NewPost from '../Tabs/NewPost';
+//mport NewPost from '../Tabs/NewPost';
 
 const DrawerIcon = (props) => {
   const drawerToggleHandler = () => {
@@ -19,12 +19,15 @@ const DrawerIcon = (props) => {
         <Text style={styles.text}>
           HearMeOut <FontAwesome5Icon name="bullhorn" size={22} />
         </Text>
-        <FontAwesome5Icon
-          style={styles.icon1}
-          name="plus"
-          onPress={() => props.navigation.navigate('NewPost')}
-          size={20}
-        />
+        {(props.user.status === 'Recruiter' ||
+          props.user.status === 'Organizer') && (
+          <FontAwesome5Icon
+            style={styles.icon1}
+            name="plus"
+            onPress={() => props.navigation.navigate('NewPost')}
+            size={20}
+          />
+        )}
       </View>
     </>
   );
